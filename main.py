7e216c2f7ec76a82861c0a13b418a622d67aef23
ikaramatov <ibragim11197@gmail.com>
2025-08-03 06:47:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import sys, os
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtGui import QIcon
 from ui.chat_window import ChatWindow
+from memory.memo_session import log_app_launch
+from memory.memo_session import log_app_close
+
 
 def main():
     app = QApplication(sys.argv)
-
+    log_app_launch()
+    app.aboutToQuit.connect(log_app_close)
     if sys.platform == "darwin":
         icon_path = os.path.abspath("assets/dockpic.png")
         app.setWindowIcon(QIcon(icon_path))
@@ -17,3 +21,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
